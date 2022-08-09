6f82401ffbbc9fd21530d00ddf6b5ba9a517bc5f
Jaryd Peters <jaryd.peters@distek.com>
2022-08-09 08:34:28 -0500

    Editing readme and commenting on 01-12

--- C/Textbook/01-12/main.c
+++ C/Textbook/01-12/main.c
@@ -11,33 +11,44 @@
 #define OUTSIDE 0
 #define INSIDE  1
 
+//state flag to mark if currently "inside" (within letters) or "outside" (within spaces) of a word
 int state = OUTSIDE;
+//buffer to hold stdin while its evaluated to be pushed to stdout
 int buffer;
 
 int main(void)
 {
     printf("Enter text to be printed one word per line:\n");
 
+    //keep pulling from stdin until EOF
     while((buffer = getchar()) != EOF)
     {
+        //found a space
         if(buffer == ' ')
         {
+            //only the first space after a word is important
             if(state == INSIDE)
             {
+                //push newline to stdout
                 putchar('\n');
+                //set state to outside so repeated spaces don't get their own newlines
                 state = OUTSIDE;
             }
         }
+        //first letter after spaces
         else if(state == OUTSIDE)
         {
+            //set state to inside a word
             state = INSIDE;
+            //release first letter of word to stdout
             putchar(buffer);
         }
+        //repeated letters
         else
         {
+            //release to stdout
             putchar(buffer);
         }
     }
-    
     return 0;
 }
